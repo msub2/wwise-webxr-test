9e856796e7508d691e2b13390eb1a97cacab5739
Daniel Adams <msub2official@gmail.com>
2023-08-07 22:19:21 -0400

    Update to use organizeNamespace function

--- src/wwise-components.js
+++ src/wwise-components.js
@@ -21,45 +21,7 @@ AFRAME.registerComponent('wwise', {
 
   main: function () {
     // Organize functions to live under namespaces e.g. AK.SoundEngine.Func instead of AK.SoundEngine_Func
-    // There has to be a better way to do this
-    const namespaces = {
-      Comm: { value: {}, enumerable: true },
-      Instrument: { value: {}, enumerable: true },
-      MemoryMgr: { value: {}, enumerable: true },
-      Monitor: { value: {}, enumerable: true },
-      MusicEngine: { value: {}, enumerable: true },
-      SoundEngine: { value: {
-        DynamicDialogue: {},
-        DynamicSequence: {},
-        Query: {}
-      }, enumerable: true },
-      SpatialAudio: { value: {
-        ReverbEstimation: {},
-      }, enumerable: true },
-      SpeakerVolumes: { value: {}, enumerable: true },
-      StreamMgr: { value: {}, enumerable: true },
-    };
-    const nsKeys = Object.keys(namespaces);
-    Object.keys(AK).forEach(key => {
-      for (const nsKey in nsKeys) {
-        if (key.includes(nsKeys[nsKey])) {
-          const values = key.split('_');
-          if (values.length == 2) {
-            const namespace = values[0];
-            const item = values[1];
-            namespaces[namespace].value[item] = AK[key];
-          } else if (values.length == 3) {
-            const namespace1 = values[0];
-            const namespace2 = values[1];
-            const item = values[2];
-            namespaces[namespace1].value[namespace2][item] = AK[key];
-          }
-          delete(AK[key]);
-          break;
-        }
-      }
-    })
-    AK = Object.defineProperties(AK, namespaces);
+    AK.organizeNamespaces();
 
     // Initialize all Wwise components
     AK.MemoryMgr.Init();
